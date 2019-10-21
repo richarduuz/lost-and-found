0b0e6f8a7d14e03d43b6bc5d4b0f4b84f1c34492
Tiantong  Li <glorialtt0118@gmail.com>
2019-10-21 19:11:37 +1100

    upload image

--- app/src/main/java/com/example/maptest/GoToPublishItems.java
+++ app/src/main/java/com/example/maptest/GoToPublishItems.java
@@ -34,8 +34,10 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Timer;
@@ -48,13 +50,18 @@ import static android.app.Activity.RESULT_OK;
 public class GoToPublishItems extends AppCompatActivity {
     private String Location;
     private String Found;
+    private String image_string;
     private EditText Name;
     private EditText Description;
+    private EditText Phone;
+    private EditText ContactName;
     private byte[] Image;
     private Button publish;
     private Button selectIamge;
+    private Button camerabtn;
     private ImageView upload_imageview;
     public static final int GALLERY_REQUEST_CODE = 1;
+    public static final int CAMERA_REQUEST_CODE = 0;
     private Uri uploadUri;
 
     @Override
@@ -65,6 +72,8 @@ public class GoToPublishItems extends AppCompatActivity {
         Location=intent.getStringExtra("Location");
         Found=intent.getStringExtra("Found");
         Name=findViewById(R.id.item_name);
+        Phone = findViewById(R.id.item_phone);
+        ContactName = findViewById(R.id.item_contact);
         Description=findViewById(R.id.item_description);
         publish=findViewById(R.id.gotopublish);
         publish.setOnClickListener(new View.OnClickListener(){
@@ -72,7 +81,10 @@ public class GoToPublishItems extends AppCompatActivity {
             public void onClick(View view){
                 String name=Name.getText().toString().trim();
                 String description=Description.getText().toString().trim();
-                upload(name,description,Location);
+                String phone = Phone.getText().toString().trim();
+                String contact = ContactName.getText().toString().trim();
+
+                upload(name,description,Location, phone, contact, image_string);
                 Toast.makeText(GoToPublishItems.this, "successfully added", Toast.LENGTH_LONG).show();
                 Timer timer=new Timer();
                 TimerTask task1=new TimerTask() {
@@ -93,9 +105,28 @@ public class GoToPublishItems extends AppCompatActivity {
         selectIamge.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("ButtonClick", "button click");
                 Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(intent, GALLERY_REQUEST_CODE);
+            }
+        });
+
+        camerabtn = findViewById(R.id.camerabtn);
+        camerabtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                File image = new File(getExternalCacheDir(), "lostphoto.jpg");
+                try {
+                    if (image.exists()) {
+                        image.delete();
+                        image.createNewFile();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                uploadUri = FileProvider.getUriForFile(getApplicationContext(), getPackageName() + ".fileprovider", image);
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, uploadUri);
+                startActivityForResult(camera_intent, CAMERA_REQUEST_CODE);
             }
         });
 
@@ -106,10 +137,11 @@ public class GoToPublishItems extends AppCompatActivity {
         }
     }
 
-    public void upload(String name, String description, String Location){
+    public void upload(String name, String description, String Location, String phone, String contact, String image){
         Map<String, String> lostThing=new HashMap<>();
         lostThing.put("Name",name);
         lostThing.put("Description",description);
+        lostThing.put("Image", image);
         lostThing.put("Found",Found);
         FirebaseFirestore db= FirebaseFirestore.getInstance();
         db.collection(Location)
@@ -134,6 +166,11 @@ public class GoToPublishItems extends AppCompatActivity {
         if (requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK && null != data) {
             uploadUri = data.getData();
             upload_imageview.setImageURI(uploadUri);
+            uriToByteArray(uploadUri);
+        }
+        else if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK){
+            upload_imageview.setImageURI(uploadUri);
+            uriToByteArray(uploadUri);
         }
         else {
             Log.e("Gallery", "Gallery upload error");
@@ -148,6 +185,27 @@ public class GoToPublishItems extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void uriToByteArray(Uri uri) {
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        try {
+            // Read BitMap by file path.
+            Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        } catch (Exception e) {
+            Log.d("Image", "Image path error");
+        }
+
+        byte[] byteArray = stream.toByteArray();
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            image_string = Base64.getEncoder().encodeToString(byteArray);
+        }
     }
 
 }
